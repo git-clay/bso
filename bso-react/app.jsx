05ec264edf89189f1948e70e1a617d4e24fa0d5b
git-clay <ce.smith5525@gmail.com>
2017-03-01 11:48:25 -0700

    structuring js instead of jsx

--- bso-react/app.jsx
+++ bso-react/app.jsx
@@ -1,9 +1,25 @@
-var React = require('react');
-var router = require('./stores/RouteStore.react.jsx').getRouter();
-window.React = React;
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Router, Route, IndexRoute, hashHistory} from 'react-router';
 
-router.run(function (Handler, state) {
-  React.render(
-  	<Handler/>, document.getElementById('content')
-  );
-});
+
+import Layout from './pages/Layout'
+//routing video
+// https://www.youtube.com/watch?v=1iAG6h9ff5s&list=PLoYCgNOIyGABj2GQSlDRjgvXtqfDxKm5b&index=6
+
+const app = document.getElementById('app')
+
+ReactDOM.render(
+	<Router history={hashHistory}>
+		<Route path='/' component={Layout}
+		</Route>
+	</Router>
+);
+
+
+/*********** 
+
+Figure out structure. Currently pulling in `~.react.jsx` files and 
+normal js files. Determine which to use and keep it consistant
+
+*************/
---
+++ bso-react/components/layout/Footer.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+
+export default class Footer extends React.Component {
+  render() {
+    return (
+      <footer style={footerStyles}>
+        <div class="row">
+          <div class="col-lg-12">
+          </div>
+        </div>
+      </footer>
+    );
+  }
+}
